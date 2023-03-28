cd0e2c463d1c593d5281e2ab1595b1ba7e9b536c
LocNlu <18130128@st.hcmuaf.edu.vn>
2023-03-28 08:09:26 +0700

    Initial commit

---
+++ src/main/java/com/example/cinemaweb_client/CinemaWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemaweb_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaWebClientApplication.class, args);
+    }
+
+}
